6cfa8c5ed5cfe94183ec9eef06c1c4c6eee033b0
nicocoffo <nicocoffo@gmail.com>
2018-11-30 16:35:09 +1000

    Add basic plex integration

--- src/notifications.py
+++ src/notifications.py
@@ -4,15 +4,24 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+from plexapi.myplex import MyPlexAccount
+from threading import Timer
+
 ExceptionTemplate = string.Template(
 """Server encountered an exception during execution:
   Message: $msg
   Trace:
 $trace""")
 
+def plex_scan(config):
+    account = MyPlexAccount(config['username'], config['password'])
+    plex = account.resource(config['server']).connect()
+    plex.library.refresh()
+
 class Notifications:
     def __init__(self, config):
         self.email = config['email']
+        self.plex = config['plex']
 
     def send(self, subject, body):
         msg = MIMEMultipart()
@@ -35,36 +44,5 @@ class Notifications:
         self.send(subject, msg)
 
     def update_services(self, target):
-        return "No services to update"
-
-#def plex_scan(config):
-#    account = MyPlexAccount(config['username'], config['password'])
-#    plex = account.resource(config['server']).connect()
-#    plex.library.refresh()
-#
-#def trigger_plex_scan(job):
-#    if not 'plex' in job.config:
-#        return
-#    #Timer(20, plex_scan, args=[job.config['plex']])
-#    plex_scan(job.config['plex'])
-#def job_failure_notification(job):
-#    if not 'email' in job.config:
-#        return
-#
-#    subject = "Converter - Failed Job: %s" % job.name
-#    msg = "Issue encountered in job: %s" % job
-#    msg += "\nSTDOUT:\n%s" % job.stdout
-#    msg += "\nSTDERR:\n%s" % job.stderr
-#    send_email(subject, msg, job.config['email'])
-#
-#def job_success_notification(job):
-#    # TODO: Timing info, optional message
-#    if not 'email' in job.config:
-#        return
-#
-#    subject = "Converter - Successful Job: %s" % job.name
-#    msg = "Job completed: %s" % job
-#    send_email(subject, msg, job.config['email'])
-#from time import sleep
-#from plexapi.myplex import MyPlexAccount
-#from threading import Timer
+        Timer(20, plex_scan, args=[self.plex])
+        return "Plex updated"
